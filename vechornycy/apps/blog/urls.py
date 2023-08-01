7d45a532b64a5abba136197eea2bb4ffa0468bcd
ivan-kononenko <ivan.a.kononenko@gmail.com>
2023-08-01 16:02:57 +0300

    Templates pt.1

--- vechornycy/apps/blog/urls.py
+++ vechornycy/apps/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.main_page, name="main page"),
+    path("<int:post_id>/", views.detail, name="detail"),
 ]
--- vechornycy/apps/blog/views.py
+++ vechornycy/apps/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import HttpResponse, render
+from .models import Post, Event
+
 
 def main_page(request):
-    return HttpResponse("You are currently visiting main page!")
+    latest_post_list=Post.objects.order_by("-publication_date")[:3]
+    context = {"latest_psot_list" : latest_post_list}
+    return render(request, "blog/main_page.html", context)
+
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
 
 
